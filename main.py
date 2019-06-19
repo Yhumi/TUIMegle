8065b1c0e978cdc6c1896904f90c82f566420d7a
Nicole <nicole@cynicalpopcorn.me>
2019-06-19 23:14:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,145 @@
+import npyscreen
+import curses
+import threading
+from omeglebot import OmegleBot
+from pyomegle import OmegleClient, OmegleHandler
+import time
+import json
+
+# Redirect stdout and stderr to files
+import sys
+sys.stdout = open('logging/stdout.log', 'w')
+sys.stderr = open('logging/stderr.log', 'w')
+
+class omegleForm(npyscreen.FormBaseNew):
+    def create(self):
+        #Override handlers for the messagebox
+        handler = {
+            curses.ascii.NL: self.sendMessage,
+            curses.ascii.CR: self.sendMessage,
+            curses.KEY_ENTER: self.sendMessage
+        }
+
+        #The chatbox
+        self.Chat = self.add(omegleChat, name="Chat", max_height=40)
+
+        #Create the message box and add the handlers
+        self.Message = self.add(omegleMessageBox, name="Message", max_height=6)
+        self.Message.entry_widget.add_handlers(handler)
+
+    def sendMessage(self, _input):
+        #Get val
+        value = self.Message.value
+        self.Message.value = ""
+
+        #Send it to the app above
+        self.parentApp.onNewMessage(value)
+
+        #Ensure an update is ran regardless
+        self.display()
+
+class omegleApplication(npyscreen.NPSAppManaged):
+    def onStart(self):
+        #Create form
+        self.form = self.addForm('MAIN', omegleForm, name='Omegle')
+        
+        #Pre-creation of bot
+        self.form.Chat.entry_widget.buffer(["Creating OmegleBot..."])
+        self.omegleBot = None
+        self.client = None
+
+        #Set it up
+        self.createBot()
+
+    def createBot(self):
+        #If there's an existing bot (i.e. /reload was ran) we'll kill the old one first
+        if self.omegleBot != None:
+            self.omegleBot.kill()
+            self.client.disconnect()
+            self.form.display()
+
+        #Open the setup.json and load it
+        try:
+            with open('setup.json') as json_data:
+                clientdata = json.load(json_data)
+        
+        #If no file exists, defaults are hardcoded so it doesn't break everything
+        except (OSError, IOError):
+            #Defaults
+            clientdata = {}
+            clientdata["startLine"] = ""
+            clientdata["topics"] = []
+
+            #Warn user
+            self.updateChat("You have not created a setup.json file. Checkout the example version as this allows for topics & an optional automatic message. TUIMegle will continue with default settings in 5 seconds.")
+            time.sleep(5)
+
+        #Get the startline (or set to None)
+        if clientdata["startLine"] == "":
+            startline = None
+        else:
+            startline = clientdata["startLine"]
+
+        #Reading from new setup
+        self.omegleBot = OmegleBot(self.form, True, startline)
+
+        #Creating client with topics from file
+        self.form.Chat.entry_widget.buffer(["Connecting..."])
+        self.form.display()
+
+        self.client = OmegleClient(self.omegleBot, wpm=42, lang='en', topics=clientdata["topics"])
+        self.client.start()
+
+    #Acts as a command handler
+    def onNewMessage(self, message):
+        #If this manages to get sent with nothing, nothing will happen
+        if message.strip() == "":
+            pass
+
+        #This command (/next) will move onto a new conversation
+        elif message.strip() == "/next":
+            self.client.next()
+
+        #Stops the application
+        elif message.strip() == "/exit":
+            self.omegleBot.kill()
+            self.form.chat = []
+            self.client.disconnect()
+
+            self.form.Chat.entry_widget.buffer(["Exiting... Bye!"])
+            self.form.display()
+            time.sleep(5)
+            exit(0)
+
+        #Recreates the bot - allowing you to update the setup.json
+        elif message.strip() == "/reload":
+            self.createBot()
+
+        #Anything else will be sent as a message
+        else:
+            self.client.send(message.strip())
+
+            outstring = "You: " + message.strip()
+            self.updateChat(outstring)
+
+    #This will use the form's
+    def updateChat(self, outstring):
+        #Upper line length
+        lengthcap = self.form.Chat.width - 8
+
+        #Cut the string into chunks of lengthcap if needed
+        if len(outstring) > lengthcap:
+            self.form.Chat.entry_widget.buffer([outstring[i:i+lengthcap] for i in range(0, len(outstring), lengthcap)])
+        else:
+            self.form.Chat.entry_widget.buffer([outstring])
+
+#Boxtitles for the widgets (makes it look prettier)
+class omegleChat(npyscreen.BoxTitle):
+    _contained_widget = npyscreen.BufferPager
+
+class omegleMessageBox(npyscreen.BoxTitle):
+    _contained_widget = npyscreen.Textfield
+
+#Start the app
+if __name__ == "__main__":
+    Application = omegleApplication().run()
---
+++ omeglebot.py
@@ -0,0 +1,130 @@
+from pyomegle import OmegleClient, OmegleHandler
+import time
+import threading
+
+class OmegleBot(OmegleHandler):
+    def __init__(self, npform, auto_reconnect=False, start_message=None):
+        #Do the necessary default setup
+        super(OmegleBot, self)
+
+        #Create a link to the form where messages are sent/displayeds
+        self.form = npform
+
+        #Setup variables
+        self.auto_reconnect = auto_reconnect
+        self.start_message = start_message
+
+    #Will stop the bot from auto-reconnecting
+    def kill(self):
+        self.auto_reconnect = False
+
+    #Called while waiting for a stranger
+    def waiting(self):
+        #Defaults
+        super(OmegleBot, self).waiting()
+
+        #Clear the chat for a connecting message.
+        self.form.Chat.entry_widget.clearBuffer()
+        self.form.Chat.entry_widget.buffer(["Searching for a stranger..."])
+
+        #Update the form
+        self.form.display()
+
+    #Runs when the bot connects to a stranger
+    def connected(self):
+        #Defaults
+        super(OmegleBot, self).connected()
+
+        #Reset the chat so the old conversation isn't there
+        self.form.Chat.entry_widget.clearBuffer()
+        self.form.Chat.entry_widget.buffer(["Connected to a stranger."])
+
+        #Send a start message if that's set
+        if self.start_message != None:
+            self.omegle.send(self.start_message)
+            outstring = "You: " + self.start_message
+            self.updateChat(outstring)
+
+        #Update the form
+        self.form.display()
+
+    #Whenever a message is receieved
+    def message(self, message):
+        #Default behaviour
+        super(OmegleBot, self).message(message)
+
+        #Create the "outstring" and add it to the form
+        outstring = "Stranger: " + message
+        self.updateChat(outstring)
+
+        #This removes the "stranger is typing..." message on the footer
+        self.form.Chat.footer = ""
+
+        #Update the form
+        self.form.display()
+
+    #If common likes are established this is called
+    def common_likes(self, likes):
+        #default behaviour
+        super(OmegleBot, self).common_likes(likes)
+
+        #Output to the chat the shared likes
+        outstring = "You both like: %s" % ", ".join(likes)
+        self.updateChat(outstring)
+
+        #Update the form
+        self.form.display()
+
+    #This is called when the stranger starts typing
+    def typing(self):
+        #Default behaviour
+        super(OmegleBot, self).typing()
+
+        #Add a footer to the chat widget
+        self.form.Chat.footer = "Stranger is typing..."
+
+        #Update the form
+        self.form.display()
+
+    #This is called when they stop typing without sending a message
+    def stopped_typing(self):
+        #Default behaviour
+        super(OmegleBot, self).stopped_typing()
+
+        #Removes the footer
+        self.form.Chat.footer = ""
+        
+        #Update the form
+        self.form.display()
+
+    #This does nothing I don't know why it's here if I'm honest. For some reason it now uses a tempban rather than a captcha 
+    #Suppose it stops errors? I really don't want to remove this as something will break
+    def captcha_required(self):
+        print("Solve a captcha please!")
+        super(OmegleBot, self).captcha_required()
+        self.form.display()
+
+    #Called when disconnected by the other user (i.e. they leave :c)
+    def disconnected(self):
+        #Clear the chat and tell the user it's disconnected.
+        self.form.Chat.entry_widget.clearBuffer()
+        self.form.Chat.entry_widget.buffer(["Disconnected!"])
+
+        #Update that form
+        self.form.display()
+
+        #Auto reconnection handling
+        if self.auto_reconnect:
+            time.sleep(2)
+            self.omegle.start()
+
+    #This will use the form's
+    def updateChat(self, outstring):
+        #Upper line length
+        lengthcap = self.form.Chat.width - 8
+
+        #Cut the string into chunks of lengthcap if needed
+        if len(outstring) > lengthcap:
+            self.form.Chat.entry_widget.buffer([outstring[i:i+lengthcap] for i in range(0, len(outstring), lengthcap)])
+        else:
+            self.form.Chat.entry_widget.buffer([outstring])
